d250b43cbaaf9f6da33f19309e70d8023176b8bb
Вадим <kantora_70@mail.ru>
2023-12-26 09:20:28 +0500

    Docker

--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class Item(models.Model):
+    objects = None
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.IntegerField(default=0)
